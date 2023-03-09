26dfcfaf518750ec59d90399ba266344be9a6d16
jaisherma <79637980+jaisherma@users.noreply.github.com>
2023-03-09 12:46:40 +0530

    Delete BreadcrumbDemo directory

--- BreadcrumbDemo/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BreadcrumbDemo.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
