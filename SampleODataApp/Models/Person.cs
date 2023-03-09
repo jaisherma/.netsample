fe86c6756754d5cf638a378f04d40267c6945a15
jaisherma <79637980+jaisherma@users.noreply.github.com>
2023-03-09 12:49:20 +0530

    Delete SampleODataApp directory

--- SampleODataApp/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace SampleODataApp.Models
-{
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-}
